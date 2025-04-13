ab9e04a856655d65d080c82bf0877157f3cbca48
SheikhQais <sheikhqais27@gmail.com>
2025-04-14 04:23:50 +0500

    Sortinf list and printing the square of items in the list

--- list_sort1.py
+++ list_sort1.py
@@ -1,6 +1,14 @@
-li = [2, 3, 2, 4, 3]
-for i in range(len(li) + 1):
-    for j in range(len(li)+ 1):
-        if li[i] == li[j]:
-            li.remove(li[j])
-print(li)
+list_ = [4,5, 6, 5,6,3,7,8 ,9]
+unique = [] # declaring new list for sorting
+i =0 # initializing variable for control of loop flow
+print("list after sorting and square of items is:")
+print(list_)
+while i < len(list_):
+    if list_[i] not in unique:
+        unique.append(list_[i])
+    i += 1
+for i in range(len(unique)):
+    unique[i] = unique[i]**2
+unique.sort()
+print("New sorted list with square of items is:")
+print(unique)
