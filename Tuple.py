a670d387f67dc31f3dfa94e71e5acf95f6a9afd4
SheikhQais <sheikhqais27@gmail.com>
2025-04-15 01:56:38 +0500

    Practice Accessing items in tuple and make changes in tuple

--- Tuple.py
+++ Tuple.py
@@ -22,3 +22,43 @@ print(type(list1), list1)
 
 #Accessing Tupple Item
 print(list1[3], type(list1))
+
+print(list1[-3])
+print(list1[-1])
+print(list1[1:5])
+print(list1[-5:])
+print(list1[:6])
+
+if "Cherry" in rup:
+    print("Yes, Cherry is in the tupple")
+else:
+    print("No, It's not in the tupple")
+    
+#You cannot change the value in the tupple, But there is a workaround,
+# you will convert tupple into list, make changes and then convert it back to tupple.
+
+Tupple1 = ('Orange', 'Guava', 'Stawberry', 'Blue-Berry')
+print("Before: ", Tupple1)
+List2 = list(Tupple1)
+List2[3]= 'Kiwi'
+Tupple1 = tuple(List2)
+print('After: ',Tupple1)
+
+# Same, as Tupple is unchangeable or immuable, there is no append() built-in fuction to add item
+# You will have to convert tuple into list and then add the item and convert back into tuple
+
+Tupple2 = ('lelelmke', 'judshsd', 'kidjuhsdu','jjndsjnds')
+print("Before: ",Tupple2)
+list5 = list(Tupple2)
+list5.append('Orange')
+Tupple2 = tuple(list5)
+print("After ",Tupple2)
+
+#You can also add tuple with tuple: tuple1 + tuple2
+# Make a new tuple (tuple2) with items to add in tuple and add it with tuple1
+
+Tupple3 = (1,2,3,4,5,6)
+print("Before: ",Tupple3)
+Tupple4 = (8,9,8,7,7)
+Tupple3 = Tupple3 + Tupple4
+print("After: ",Tupple3)
