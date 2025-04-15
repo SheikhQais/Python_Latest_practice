8c2683a5f8f2bf543c111a9577a55fb4f7026ac3
SheikhQais <sheikhqais27@gmail.com>
2025-04-16 04:29:43 +0500

    Joining Tuples, problem#2, given by GPT

---
+++ tuple_looping_join.py
@@ -0,0 +1,8 @@
+fruits1 = ('apple', 'banana', 'mango')
+fruits2 = ('orange', 'grape')
+
+all_fruits = fruits1 + fruits2  #Joining the two tuples into a new single tuple
+
+print(all_fruits)
+for x in all_fruits:
+    print(x.upper())    #Converting items in Upper Case and Print them
