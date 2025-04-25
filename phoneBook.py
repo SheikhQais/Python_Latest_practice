bfe8ea82dca1e47629020c7710e2b39cdb47cbdc
SheikhQais <sheikhqais27@gmail.com>
2025-04-25 23:11:11 +0500

    Simple While Loop

--- phoneBook.py
+++ phoneBook.py
@@ -8,3 +8,10 @@ phoneBook.update({'junaid':'03232345670'})
 phoneBook.pop('usman')
 for x,y in phoneBook.items():
     print(f"{x.title()} : {y}")
+    
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
