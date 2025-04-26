0008d843d95cdb9a6ace3098f67335b33bbd223f
SheikhQais <sheikhqais27@gmail.com>
2025-04-27 04:47:13 +0500

    Practice multiple keyword arguments in function

--- Functions_Practice.py
+++ Functions_Practice.py
@@ -7,3 +7,37 @@ def greet_people(*people):
         print(f"Hello {x}")
 
 greet_people('Ali', 'Zara', 'Usman')
+
+# Create a function student_profile that accepts name, age, and city as keyword arguments (like name='Ali', etc.).
+# Inside the function, print each key and value nicely formatted.
+
+# Example Call:
+# student_profile(name="Ali", age=21, city="Lahore")
+print('\n')
+def student_profile(**arg):
+    for x, y in arg.items():
+        print(f"{x} : {y}")
+        
+student_profile(name = 'Ali', age = 23, city = 'Lahore')
+
+# Write a function called sum_marks that:
+# Takes a list of marks as a parameter.
+# Adds all the marks together.
+# Returns the total sum.
+
+# marks = [85, 90, 78, 92]
+# sum_marks(marks)
+# Expected Output:
+# Total marks: 345
+print('\n')
+
+def sum_marks(marks):
+    results = 0
+    for x in range(len(marks)):
+        results += marks[x]
+        x += 1
+    return results
+
+marks = [85, 90, 78, 92]
+print(sum_marks(marks))
+
