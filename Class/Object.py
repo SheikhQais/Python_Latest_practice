d027db7b33479a1fbadc6f627d614b3566726d13
SheikhQais <sheikhqais27@gmail.com>
2025-04-30 06:55:41 +0500

    practiced deleting object attribute and object of class

--- Class/Object.py
+++ Class/Object.py
@@ -17,7 +17,45 @@ class person:
         
     def __str__(self):                      
         return f'{self.name} {self.age}'
+    
+# The self parameter is a reference to the current instance of the class, 
+# and is used to access variables that belong to the class.
+# It does not have to be named self, you can call it whatever you like, 
+# but it has to be the first parameter of any function in the class:
+    def introduce(self):
+        return f'Hello My name is: {self.name} and my age is: {self.age}'
+    
+    def callName(abc):
+        return f'My name is {abc.name}'    
 p1 = person('Waleed',23)
 # print(p1.name,p1.age)
 
+p2 = person('Qais',24)
+
 print(p1)
+print(p1.introduce())
+print(p1.callName())
+
+# You can modify properties on objects like this:
+p1.age = 40
+print(p1.age)
+
+print(p2.introduce())
+print(p2.age)
+
+# You can delete properties on objects by using the del keyword:
+
+del p1.age
+# print(p1.age)
+
+# You can delete objects by using the del keyword:
+
+del p1
+print(p1)
+
+# The pass Statement
+# class definitions cannot be empty, but if you for some reason have -
+# a class definition with no content, put in the pass statement to avoid getting an error.
+# Example
+class Car:
+  pass
