e3cae1d4577d929f4d2d7d52e7fc857c5a9792b9
SheikhQais <sheikhqais27@gmail.com>
2025-06-11 06:21:03 +0500

    File Handling Basic

--- Class/File_Handling.py
+++ Class/File_Handling.py
@@ -1,2 +1,30 @@
+# f = open('D:/Python_Latest_practice/Class/myfile.txt', 'a')
+# content  = f.read()
+# print(content)
 
-fle = open('txt,csv','rt')
+# f.append('Occupation: Software Developer')
+# print(f.read())
+# f.close()
+
+# Step 1: Read the file line-by-line and print each line
+# with open('D:/Python_Latest_practice/Class/myfile.txt') as f:
+#     for x in f:
+#         print(x)
+
+with open('D:/Python_Latest_practice/Class/myfile.txt', 'r') as f:
+    content = f.read()
+
+update_content = content.replace('City: New York', 'City: San Francisco')
+
+# Step 2: Check if the line already exists, and only append if it's not there
+if 'Occupation: Software Developer' not in content:
+    with open('D:/Python_Latest_practice/Class/myfile.txt', 'a') as f:
+        f.write('\nOccupation: Software Developer')
+
+# Step 3: Read and writing updated file content to the file content
+with open('D:/Python_Latest_practice/Class/myfile.txt', 'w') as f:
+    f.write(update_content)
+
+# Step 4: Read and print the entire file content    
+with open('D:/Python_Latest_practice/Class/myfile.txt') as f:
+    print(f.read())
